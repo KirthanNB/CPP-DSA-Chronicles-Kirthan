cd7dee8eb33910572b9a568e987d7026224e48e2
KirthanNB <kirthannb@gmail.com>
2025-04-14 22:45:30 +0530

    Day_44 Challenge Completed

---
+++ Some_Concepts/2D Arrays/Day_44-Linear_Search.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int solution(vector<vector<int>> matrix, int target){
+    int rows = matrix.size();
+    int cols = matrix[0].size(); 
+    for(int i=0; i<rows; i++){
+        for(int j=0; j<cols; j++){
+            if(matrix[i][j] == target){
+                cout<<"Target found at: "<<i<<", "<<j;
+                return 0;
+            }
+        }
+    }
+    cout<<"Target element dont exist in the matrix.";
+    return 0;
+}
+int main(){
+    int m, n, temp, target;
+    cout<<"Enter the order of matrix: ";
+    cin >> m >> n;
+    vector<vector<int>> matrix(m, vector<int>(n));
+    cout << "Enter " << n*m << " elements: \n";
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cin>> matrix[i][j];
+        }
+    }
+    cout<<"Enter target member: ";
+    cin>>target;
+    solution(matrix, target);
+    return 0;
+}
